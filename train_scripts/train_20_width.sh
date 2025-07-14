6f57a53049909338da2e229a32bc266e97e46b9e
kirillutyashev <kirillut04@gmail.com>
2025-07-14 12:52:14 +0300

    Added the ability to run scripts on VM

--- train_scripts/train_20_width.sh
+++ train_scripts/train_20_width.sh
@@ -20,5 +20,5 @@ do
   echo "20 by ${width}, ${num_agents}"
   python -u ../run_experiments.py \
   --algorithm ${algo} --width ${width} --length 20 --num_agents ${num_agents} --seed 42069 --timesteps 1000000 --apple_life 5 \
-  --s_target 0.16 --batch_size 256 --alpha 0.00125 --hidden_dim 128 --num_layers 4
+  --s_target 0.16 --batch_size 256 --alpha 0.00125 --hidden_dim 256 --num_layers 4
 done
