f6d4947cc34c8a73d43efebd3ea310e66f367bb5
kirillutyashev <kirillut04@gmail.com>
2025-07-14 10:26:17 +0300

    Pipeline for running 2D experiments

--- evaluate_policies.py
+++ evaluate_policies.py
@@ -1,25 +1,30 @@
-from configs.config import EnvironmentConfig
 from policies.random_policy import random_policy
 from policies.nearest import nearest_policy
 from configs.config import EnvironmentConfig
 from orchard.algorithms import spawn_apple, despawn_apple
 from value_function_learning.train_value_function import evaluate_policy, make_baseline_factory
 
-if __name__ == "__main__":
+
+def evaluate_factory(length, width, num_agents):
     env_config = EnvironmentConfig(
         s_target=0.16,
         apple_mean_lifetime=5,
-        length=12,
-        width=12,
+        length=length,
+        width=width,
         spawn_algo=spawn_apple,
         despawn_algo=despawn_apple
     )
 
-    baseline_metrics = evaluate_policy(
+    return evaluate_policy(
         env_config,
-        num_agents=7,
+        num_agents=num_agents,
         agent_factory=make_baseline_factory(random_policy),
         timesteps=10000,
         seed=42069
     )
-    print(baseline_metrics)
+
+
+if __name__ == "__main__":
+    widths = [1, 3, 5, 8]
+    for width in widths:
+        print(evaluate_factory(20, width, 4))
