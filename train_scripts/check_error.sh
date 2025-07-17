02dbe5787d311339ba0eaf263279307614a13057
kirillutyashev <kirillut04@gmail.com>
2025-07-17 14:03:12 +0300

    Added the ability to run scripts on VM

--- train_scripts/check_error.sh
+++ train_scripts/check_error.sh
@@ -9,6 +9,6 @@ do
   echo "${length} by ${width}, ${num_agents}"
   python ../run_experiments.py \
   --algorithm ${algo} --width ${width} --length ${length} --num_agents ${num_agents} --seed 42069 --timesteps 600000 --apple_life 5 \
-  --s_target 0.16 --batch_size 256 --alpha 0.00125 --hidden_dim 64 --num_layers 4
+  --s_target 0.16 --batch_size 256 --alpha 0.00125 --hidden_dim 64 --num_layers 4 --debug True
 done
 
