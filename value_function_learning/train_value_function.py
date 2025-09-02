0d44cfc439c365c752c5d23f47df321b54d1e952
kirillutyashev <kirillut04@gmail.com>
2025-09-01 21:57:10 -0400

    Changes before Actor Critic experiments

--- value_function_learning/train_value_function.py
+++ value_function_learning/train_value_function.py
@@ -34,7 +34,7 @@ class CentralizedValueFunction(ValueFunction):
 
     def __init__(self, config: ExperimentConfig):
         """Initialize the value function algorithm."""
-        super().__init__(config, f"Centralized-<{config.train_config.num_agents}>_agents-_length-<{config.env_config.length}>_width-<{config.env_config.width}>_s_target-<{config.env_config.s_target}>-apple_mean_lifetime-<{config.env_config.apple_mean_lifetime}>-<{config.train_config.alpha}>-discount-<{config.train_config.discount}>-hidden_dimensions-<{config.train_config.hidden_dimensions}>-dimensions-<{config.train_config.num_layers}>-vision-<{config.train_config.critic_vision}>-epsilon-<{config.train_config.epsilon}>-batch_size-<{config.train_config.batch_size}>")
+        super().__init__(config, f"Centralized-<{config.train_config.num_agents}>_agents-_length-<{config.env_config.length}>_width-<{config.env_config.width}>_s_target-<{config.env_config.s_target}>-apple_mean_lifetime-<{config.env_config.apple_mean_lifetime}>-<{config.train_config.alpha}>-discount-<{config.train_config.discount}>-hidden_dimensions-<{config.train_config.hidden_dimensions}>-dimensions-<{config.train_config.num_layers}>-vision-<{config.train_config.critic_vision}>-epsilon-<{config.train_config.epsilon}>-batch_size-<{config.train_config.batch_size}>-env_cls-<{config.env_config.env_cls}>")
 
     def init_agents_for_eval(self) -> Tuple[List[SimpleAgent], AgentController]:
         a_list = []
