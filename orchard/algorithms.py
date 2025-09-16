84bfe317ea3453e91dcb86273c3003e32b375f8a
amandahe <xiying.he@mail.utoronto.ca>
2025-09-16 13:23:45 -0400

    added dirt spawn function and modified apple spawn

--- orchard/algorithms.py
+++ orchard/algorithms.py
@@ -16,6 +16,14 @@ def get_nearest_agent_apple_distance(apple_pos, agents):
             min_distance = dist
     return min_distance
 
+def get_nearest_agent_dirt_distance(dirt_pos, agents):
+    min_distance = float('inf')
+    for agent_ in agents:
+        dist = distance(agent_.position, dirt_pos)
+        if dist < min_distance:
+            min_distance = dist
+    return min_distance
+
 
 def despawn_apple(env, q_despawn):
     """
@@ -34,20 +42,38 @@ def despawn_apple(env, q_despawn):
     env.apples[removal_mask] -= 1
     return total_removed
 
-
-def spawn_apple(env, p_cell):
+def spawn_apple(env, p_cell, total_dirt) :
     H, L = env.apples.shape  # ★ rows = width, cols = length
 
     rand_mat = np.random.rand(H, L)  # ★ new RNG draw, same shape
-    spawn_mask = rand_mat < p_cell
-    positions = np.argwhere(spawn_mask)
-    total_spawned = spawn_mask.sum()
+    spawn_positions = rand_mat < p_cell
+    positions = np.argwhere(spawn_positions).tolist()
+    dirt_slowed_rate = 1 - (total_dirt / H * L) ^ 2
+    total_spawned = spawn_positions.sum() * dirt_slowed_rate
+    Reduced_spawned = spawn_positions.sum() - total_spawned
+    while Reduced_spawned > 0:
+        for i in positions:
+            if np.random.rand() < 0.5 and Reduced_spawned > 0:
+                positions.remove(i)
+                Reduced_spawned -= 1
     if total_spawned > 0:
         for position in positions:
             mean_distances.append(get_nearest_agent_apple_distance(position, env.agents_list))
-    env.apples[spawn_mask] += 1
+    env.apples[spawn_positions] += 1
     return total_spawned
 
+def spawn_dirt(env, p_cell):
+    L, W = env.apples.shape  # ★ rows = Length, cols = Width
+
+    rand_mat = np.random.rand(L, W)  # ★ new RNG draw, same shape
+    spawn_positions = rand_mat < p_cell
+    positions = np.argwhere(spawn_positions)
+    total_spawned = spawn_positions.sum()
+    if total_spawned > 0:
+        for position in positions:
+            mean_distances.append(get_nearest_agent_dirt_distance(position, env.agents_list))
+    env.dirt[spawn_positions] += 1
+    return total_spawned
 
 def spawn_apple_selfless_orchard(env, p_cell):
     H, L = env.apples.shape  # rows = width, cols = length
