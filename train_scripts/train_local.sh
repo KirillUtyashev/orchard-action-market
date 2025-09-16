1961a011a92dce140bc50105b2eb8ff5f3134dd5
kirillutyashev <kirillut04@gmail.com>
2025-09-15 23:08:16 -0400

    Changes to the environment

--- train_scripts/train_local.sh
+++ train_scripts/train_local.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-
 algo="Decentralized"
-num_agents=4
-length=9
-width=9
+num_agents=2
+length=6
+width=6
 
-for hidden_dim in 64
+for hidden in 16 256
 do
   echo "${length} by ${width}, ${num_agents}"
-  cd .. && python run_experiments.py \
-  --algorithm ${algo} --width ${width} --length ${length} --num_agents ${num_agents} --seed 42069 --timesteps 1000000 --apple_life 5 \
-  --s_target 0.16 --batch_size 4 --alt_vision 1 --vision 0 --alpha 0.000275 --hidden_dim ${hidden_dim} --num_layers 4 --skip 1 --epsilon 0.1
+  python ../run_experiments.py \
+  --algorithm ${algo} --width ${width} --length ${length} --num_agents ${num_agents} --seed 42069 \
+  --timesteps 1000000 --apple_life 5 --s_target 0.16 --batch_size ${num_agents} --critic_vision 0 \
+  --new_input 1 --alpha 0.000275 --hidden_dim ${hidden} --num_layers 4 --skip 1 --epsilon 0.1 --env_cls "OrchardEuclideanRewards" --new_dynamic 0
 done
-
