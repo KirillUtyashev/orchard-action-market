56ec441f6848ff8c4713643287f38bbeadee92ee
Tadd Mao <taddmao@mail.cs.toronto.edu>
2026-04-11 00:06:52 -0400

    created a new clean repo that removes all files we no longer use

--- agents/simple_agent.py
+++
@@ -1,9 +0,0 @@
-from agents.value_agent import ValueAgent
-
-
-class SimpleAgent(ValueAgent):
-    def add_experience(self, old_state, new_state, reward):
-        self.policy_value.add_experience(old_state, new_state, reward)
-
-    def get_value_function(self, state):
-        return self.policy_value.get_value_function(state[:self.policy_value.get_input_dim()])
